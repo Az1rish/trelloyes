e6a67b96657d98bcc629c7d73ea632ca7b31da64
Shane <shanemcneil19@gmail.com>
2019-08-25 16:28:22 -0700

    add STORE as a prop within index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import STORE from './store';
-console.log(STORE);
-ReactDOM.render(<App />, document.getElementById('root'));
 
+ReactDOM.render(
+    <App store={STORE} />, 
+    document.getElementById('root'));
