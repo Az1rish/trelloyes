cd6bf3cb19883bfcda13c35cb63b847af4da2ed4
Shane <shanemcneil19@gmail.com>
2019-09-07 19:10:46 -0700

    fix syntax error in constructor in App component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    store: {
-      lists: [],
-      allCards: {},
-    };
+      store: {
+        lists: [],
+        allCards: {},
+      },
+    }
   }
 
   render() {
